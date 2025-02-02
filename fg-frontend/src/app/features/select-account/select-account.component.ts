60238b0f2d7131d2591501248dfbef69ab4285d1
kfkzkozakiewicz@gmail.com <101817983+Kristoferkoza@users.noreply.github.com>
2025-02-03 00:44:26 +0100

    select-account-component-edited;button-to-add-user-implemented

--- fg-frontend/src/app/features/select-account/select-account.component.ts
+++ fg-frontend/src/app/features/select-account/select-account.component.ts
@@ -1,40 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../_models/users/user.model';
 import { UsersService } from '../../_services/users/users.service';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import {MatButtonModule} from '@angular/material/button';
 import { Router } from '@angular/router';
 import { GameService } from '../../_services/game/game.service';
 import { CommonModule } from '@angular/common';
-import { MatDivider } from '@angular/material/divider';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-select-account',
   standalone: true,
-  imports: [CommonModule, MatDivider, MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, MatButtonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, MatProgressSpinnerModule],
   providers: [UsersService],
   templateUrl: './select-account.component.html',
-  styleUrl: './select-account.component.scss'
+  styleUrl: './select-account.component.scss',
+  animations: [
+    trigger('cardAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(20px)' }),
+        animate('0.4s ease', style({ opacity: 1, transform: 'translateY(0)' }))
+      ])
+    ]),
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.3s ease-in', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
 export class SelectAccountComponent implements OnInit {
   accounts: User[] = [];
+  loading: boolean = true;
 
-  constructor(private usersService: UsersService, private router: Router, private gameService: GameService) {}
+  constructor(
+    private usersService: UsersService,
+    private router: Router,
+    private gameService: GameService
+  ) {}
 
   ngOnInit() {
-    this.accounts = [];
     this.gameService.deleteCounter();
-    this.usersService.findAll().subscribe((accounts => {
+    this.usersService.findAll().subscribe(accounts => {
       this.accounts = accounts as User[];
-    }));
+      this.loading = false
+    });
   }
 
   play(userId: string) {
     this.usersService.setSelectedAccountId(userId);
     this.router.navigate(['/clicker']);
+  }
+
+  navigateToAddUser () {
+    
   }
 }
