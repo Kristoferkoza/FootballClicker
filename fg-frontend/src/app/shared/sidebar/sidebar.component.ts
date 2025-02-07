cd0095afbe445990c246c8c834e6d987c97b32a9
kfkzkozakiewicz@gmail.com <101817983+Kristoferkoza@users.noreply.github.com>
2025-02-07 23:39:43 +0100

    showing-bonus-on-the-sidebar

--- fg-frontend/src/app/shared/sidebar/sidebar.component.ts
+++ fg-frontend/src/app/shared/sidebar/sidebar.component.ts
@@ -6,37 +6,63 @@ import { UsersService } from '../../_services/users/users.service';
 import { User } from '../../_models/users/user.model';
 import { HttpClientModule } from '@angular/common/http';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { UserKitElementConfigService } from '../../_services/userkitelementconfig/user-kitelementconfig.service';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [CommonModule, MatDividerModule, HttpClientModule, MatProgressSpinnerModule],
-  providers: [UsersService],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatDividerModule,
+        HttpClientModule,
+        MatProgressSpinnerModule,
+    ],
+    providers: [UsersService, UserKitElementConfigService],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent implements OnInit {
-  sidebarOpened = true;
-  selectedUser!: User;
+    selectedUser!: User;
+    bonus: number = 0;
+    loading: boolean = true;
 
-  loading: boolean = true;
+    constructor(
+        private router: Router,
+        private usersService: UsersService,
+        private userKitElementConfigService: UserKitElementConfigService
+    ) {}
 
-  constructor(private router: Router, private usersService: UsersService) {}
+    ngOnInit(): void {
+        const userId = this.usersService.getSelectedAccountId()!;
 
-  ngOnInit(): void {
-      this.usersService.findOne(this.usersService.getSelectedAccountId()!).subscribe(user => {
-        this.selectedUser = user as User;
-        this.loading = false;
-      });
-  }
+        this.usersService.findOne(userId).subscribe(
+            (user) => {
+                this.selectedUser = user as User;
 
-  toggleSidebar() {
-    this.sidebarOpened = !this.sidebarOpened;
-  }
+                this.userKitElementConfigService
+                    .getUserKitConfig(userId)
+                    .subscribe(
+                        (config) => {
+                            this.bonus = config.bonus;
+                            this.loading = false;
+                        },
+                        (error) => {
+                            console.error(
+                                'Błąd podczas pobierania bonusu:',
+                                error
+                            );
+                            this.loading = false;
+                        }
+                    );
+            },
+            (error) => {
+                console.error('Błąd podczas pobierania użytkownika:', error);
+                this.loading = false;
+            }
+        );
+    }
 
-  navigateTo(path: string) {
-    this.router.navigate([path]);
-  }
-
-  
+    navigateTo(path: string) {
+        this.router.navigate([path]);
+    }
 }
